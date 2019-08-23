bb4d230bae03e830e3119ebb137dc2f1e118ab27
dangwenxiang <790101181@qq.com>
2019-08-23 16:37:05 +0800

    第一次提交

---
+++ zuul/src/main/java/com/example/zuul/filter/AccessFilter.java
@@ -0,0 +1,45 @@
+package com.example.zuul.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author: dwx
+ * @create: 2019-08-21 13:52
+ **/
+public class AccessFilter extends ZuulFilter {
+    private static Logger log = LoggerFactory.getLogger(AccessFilter.class);
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest httpServletRequest = requestContext.getRequest();
+        log.debug(String.format("%s request to %s", httpServletRequest.getMethod(), httpServletRequest.getRequestURL().toString()));
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Integer a= 120, b =120, c= 200, d = 200;
+        System.out.println(a == b);
+        System.out.println(c==d);
+    }
+}
